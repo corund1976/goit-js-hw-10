fe229f4b99c8f89426b38057926b10281b88988c
Sergii Budak <corund1976@gmail.com>
2021-10-07 17:24:18 +0300

    Finish

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,9 +1,9 @@
 export default function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flag,languages`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }          
-                return response.json();
-            })
+  return fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flag,languages`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+        }          
+      return response.json();
+    })
 };
